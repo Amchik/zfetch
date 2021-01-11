0ee49795f4b3162d50f1bdf7042f1ff1486cfbf0
Amchik <am4ik1337@gmail.com>
2021-01-11 13:39:01 +0500

    fix include/zfetch-vars.h

---
+++ include/zfetch-vars.h
@@ -0,0 +1,14 @@
+
+#ifndef _ZF_H_ZFETCH_VARS
+#define _ZF_H_ZFETCH_VARS
+
+#define config_dir "/.zfetch/"
+#define default_logo "(B[m[30m      ___(B[m\n(B[m[30m     ((B[m[37m.. (B[m[30m\\(B[m\n(B[m[30m     ((B[m[33m<> (B[m[30m|(B[m\n(B[m[30m    /(B[m[37m/  \\ (B[m[30m\\(B[m\n(B[m[30m   ( (B[m[37m|  | (B[m[30m/|(B[m\n(B[m[33m  _(B[m[30m/\\ (B[m[37m__)(B[m[30m/(B[m[33m_(B[m[30m)(B[m\n(B[m[33m  \\/(B[m[30m-____(B[m[33m\\/(B[m\n\nThanks ufetch for this art. (ufetch-linux)\n"
+#define default_logo_height 7
+#define default_logo_width 13
+
+#define main_file_name "zfconfig"
+#define logo_file_name "logo"
+#define info_file_name "info"
+
+#endif
